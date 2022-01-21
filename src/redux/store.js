96cf8dbc6afeff056b0764e1a74665c5d981706b
joon <83916101+jackma914@users.noreply.github.com>
2022-01-21 18:48:18 +0900

    add
    add

--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import subscribersReducer from "./subscribers/reducer";
-import rootReducer from "./rootReducer";
-
-const store = createStore(rootReducer);
-
-export default store;
